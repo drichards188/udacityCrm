5d9e730689f9d53f51faccf891a2afadec8e7041
David Richards <50757879+drichards188@users.noreply.github.com>
2022-12-14 14:39:19 -0700

    ready for submit (#4)

--- pkg/controllers/rootHandler.go
+++ pkg/controllers/rootHandler.go
@@ -1,17 +1,14 @@
 package controllers
 
 import (
-	"crmBackend/pkg/models"
-	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
-func GetRoot(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-
-	resp := models.DetailResp{Status: 1, Msg: "Welcome to the customer API. Active routes are /customers. The customer object needed is {Id: int, Name: string, Role: string, active: bool} Create: /customers Get Single: /customers/:id Get All: /customers Update Single: /customers/:id Delete Single: /customers/:id"}
-
-	json.NewEncoder(w).Encode(resp)
-
+func ServeIndex(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/html")
+	fmt.Fprintf(w, "<h1>Welcome to the customer API. Active routes are /customers.</h1>")
+	fmt.Fprintf(w, "<p>The customer object needed is {Id: int, Name: string, Role: string, active: bool} </p>")
+	fmt.Fprintf(w, "<p>Create: /customers Get Single: /customers/:id Get All: /customers Update Single: /customers/:id</p>")
+	fmt.Fprintf(w, "<p>Delete Single: /customers/:id</p>")
 }
